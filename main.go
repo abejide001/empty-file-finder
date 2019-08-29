7ef47aa8b67dcdd117001bd5a52838203fc2afcb
abejide001 <abejidefemi1@gmail.com>
2019-08-29 13:07:57 +0100

    write to another file

--- main.go
+++ main.go
@@ -15,9 +15,17 @@ func main() {
 	  fmt.Println("error", err)
 	  return
   }
+  var names []byte
   for _, file := range files {
 	  if file.Size() == 0 { // check if file size is 0
-		  fmt.Println(file.Name())
+		  name := file.Name()
+		  names = append(names, name...) // name... converts the string to byte
+		  names = append(names, '\n')
 	  }
   }
+  if err := ioutil.WriteFile("out.ext", names, 0355); err != nil {
+	  fmt.Println(err)
+	  return
+  }
+  fmt.Printf("%s", names)
 }
